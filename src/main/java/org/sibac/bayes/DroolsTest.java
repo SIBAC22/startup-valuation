b3c86687ae5addc1c4bdf8a52208f9a24ca0db76
RomanCech <81cechroman@gmail.com>
2023-01-11 18:19:36 +0000

    Can you protect your technology rule.

--- src/main/java/org/sibac/bayes/DroolsTest.java
+++ src/main/java/org/sibac/bayes/DroolsTest.java
@@ -43,10 +43,13 @@ public class DroolsTest {
 
 			kSession.insert(new FrameProduct(1.0,"What is your TRL? [value 1-9]", "9" ));
 
-			kSession.insert(new Evidence(1.0, "Do you have competitors?", "no"));
+			kSession.insert(new Evidence(1.0, "Do you have competitors?", "yes"));
 
 			kSession.insert(new Evidence(1.0, "From 1 to 10, how good is your LTV?", "9"));
 			kSession.insert(new Evidence(1.0, "From 1 to 10, how good is your engagement rate?", "10"));
+
+
+			kSession.insert(new Evidence(1.0, "Can you protect your technology?", "yes"));
 
 
 			kSession.fireAllRules();
